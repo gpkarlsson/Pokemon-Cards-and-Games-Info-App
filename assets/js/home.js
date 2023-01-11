b1a5832431282f2bc0651377edce99d44504591d
gordonk <gpkarlsson72@gmail.com>
2023-01-10 19:48:44 -0600

    working on js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,18 @@
+var pokeName
+var pokedexNumber
+var pokeHeight
+var pokeWeight
+var pokeGen
+var searchBar = document.getElementById('searchBar');
+var tcgCardSets = document.getElementById('cardSets');
 
-var PokeBaseUrl = `https://pokeapi.co/api/v2`;
-var tcgBaseUrl = `https://api.pokemontcg.io/v2`;
+var pokeGameInput
+
+const pokeBaseUrl = `https://pokeapi.co/api/v2`;
+// name, number, type, 
+
+const tcgBaseUrl = `https://api.pokemontcg.io/v2`;
+//cards (*image*), card sets (base, 
 
 fetch(`https://pokeapi.co/api/v2/berry-flavor/sweet/`).then((response) => response.json())
 .then((data) => console.log(data));
@@ -12,6 +24,24 @@ fetch('https://api.pokemontcg.io/v2/sets/'+ setId).then((response) => response.j
 }
 getSet();
 
-function getName() {
 
+
+var poke1 = document.getElementById('pokemon1')
+
+
+var pokemonName = [];
+var pokeFetch = fetchPoke();
+
+ function fetchPoke() {
+  fetch(pokeBaseUrl + '/pokemon' + '/charizard').then((response) => response.json())
+  .then((data) => console.log(data));
 }
+
+fetchPoke();
+
+console.log(pokeFetch);
+
+poke1.innerHTML = pokeFetch;
+
+//getPokemon();
+// https://pokeapi.co/api/v2/pokemon/{id or name}/

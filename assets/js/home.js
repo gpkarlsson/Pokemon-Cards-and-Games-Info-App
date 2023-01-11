5c56cf8f2b1739596f16f60f3adb07a4540daf06
Jullian0426 <jullian0426@yahoo.com>
2023-01-10 20:08:38 -0600

    Create navbar and add basic styling to homepage. Solve merge conflicts.

--- assets/js/home.js
+++ assets/js/home.js
@@ -25,6 +25,7 @@ fetch('https://api.pokemontcg.io/v2/sets/'+ setId).then((response) => response.j
 getSet();
 
 
+<<<<<<< Updated upstream
 
 var poke1 = document.getElementById('pokemon1')
 
@@ -45,3 +46,29 @@ poke1.innerHTML = pokeFetch;
 
 //getPokemon();
 // https://pokeapi.co/api/v2/pokemon/{id or name}/
+=======
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+  
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+  
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+  
+      });
+    });
+  
+  });
+>>>>>>> Stashed changes

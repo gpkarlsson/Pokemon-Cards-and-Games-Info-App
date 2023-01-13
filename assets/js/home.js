6e6673bf4be088d2527bfeb224f9fea1dd9e66f8
gordonk <gpkarlsson72@gmail.com>
2023-01-12 21:01:23 -0600

    worked on javascript, IDs for a few html elements

--- assets/js/home.js
+++ assets/js/home.js
@@ -17,8 +17,6 @@ function getSet() {
 }
 getSet();
 
-
-
 var pokemon;
 var search;
 
@@ -26,7 +24,7 @@ function fetchPoke(search) {
 
   fetch(pokeBaseUrl + '/pokemon' + '/' + search)
     .then((response) => response.json())
-    .then ((data) => {
+    .then((data) => {
       pokemon = (data);
     })
     .then(() => console.log(pokemon))
@@ -56,7 +54,7 @@ function handleSearchFormSubmit(event) {
 
 
 // Function for results display
-function renderPokeResults () {
+function renderPokeResults() {
   // Removes past results
   const pastResults = document.getElementById('card');
   if (pastResults != null) {
@@ -70,7 +68,7 @@ function renderPokeResults () {
   var pokeWeight = (pokemon).weight;
   var pokeGen = (pokemon).generation;
   //let {urlName} = pokeName;
-  
+
   var card = document.createElement('div');
   var cardBody = document.createElement('div');
   var nameHeading = document.createElement('h2');
@@ -79,18 +77,18 @@ function renderPokeResults () {
   var heightEl = document.createElement('p');
   var weightEl = document.createElement('p');
   var genEl = document.createElement('p');
-  
+
   card.setAttribute('class', 'box');
   card.setAttribute('id', 'card');
   cardBody.setAttribute('class', 'box');
   card.append(cardBody);
-  
+
   //nameHeading.setAttribute();
   //numberHeading.setAttribute();
   //heightEl.setAttribute();
   //weightEl.setAttribute();
   //genEl.setAttribute();
-  
+
   nameHeading.textContent = (pokeName);
   numberHeading.textContent = (pokedexNumber);
   pokeImg.setAttribute('src', (pokeImgUrl));
@@ -105,77 +103,90 @@ function renderPokeResults () {
   cardBody.append(heightEl);
   cardBody.append(weightEl);
   cardBody.append(genEl);
-  
+
   //pokeContainer.innerHTML = 'This is a result box containing the information on the pokemon you searched: ' + ${urlName};
   pokeContainer.append(card);
-  }
-  
+}
 
+var base1 = document.getElementById('packImageBase1');
+var col1 = document.getElementById('packImageCol1');
+var dv1 = document.getElementById('packImageDv1');
+var neo1 = document.getElementById('packImageNeo1');
+var ex5 = document.getElementById('packImageEx5');
 
+function fetchSet1() {
+  fetch(tcgBaseUrl + '/cards?q=set.id:base1')
+    .then((response) => response.json())
+    .then((data) => {
+      cardImgUrl = (data);
+    })
+    .then(() => console.log(cardImgUrl))
+};
+fetchSet1();
 
-// EXAMPLE OF GETTING DATA FROM API USING FETCH AND DISPLAYING TO PAGE - FOLLOW LINK FOR STEPS
-//both APIs return data as object, either figure out how to pull and display data from object or convert to an array (entries) then pull and display data
+function fetchSet2() {
+  fetch(tcgBaseUrl + '/cards?q=set.id:neo1')
+    .then((response) => response.json())
+    .then((data) => {
+      cardImgUrl = (data);
+    })
+    .then(() => console.log(cardImgUrl))
+};
+fetchSet2();
 
-//getPokemon();
-// https://pokeapi.co/api/v2/pokemon/{id or name}/
+function fetchSet3() {
+  fetch(tcgBaseUrl + '/cards?q=set.id:ex5')
+    .then((response) => response.json())
+    .then((data) => {
+      cardImgUrl = (data);
+    })
+    .then(() => console.log(cardImgUrl))
+};
+fetchSet3();
 
+function fetchSet4() {
+  fetch(tcgBaseUrl + '/cards?q=set.id:dv1')
+    .then((response) => response.json())
+    .then((data) => {
+      cardImgUrl = (data);
+    })
+    .then(() => console.log(cardImgUrl))
+};
+fetchSet4();
+
+function fetchSet5() {
+  fetch(tcgBaseUrl + '/cards?q=set.id:col1')
+    .then((response) => response.json())
+    .then((data) => {
+      cardImgUrl = (data);
+    })
+    .then(() => console.log(cardImgUrl))
+};
+fetchSet4();
+
+//do for each card image
+base1.addEventListener('click', function() {
+  fetchSet1();
+  //append data to page innerHTML so on so forth
+});
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-  
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-  
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-  
-      });
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach(el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
     });
-  
   });
-
-// Submit Button
-
-
-
-
-
-// https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data#:~:text=How%20To%20Use%20the%20JavaScript%20Fetch%20API%20to,3%20Step%203%20%E2%80%94%20Handling%20POST%20Requests%20
-// be sure to read the comments to fix typos in the main article
-
-// const ul = document.getElementById('authors')
-//     const list = document.createDocumentFragment();
-//     const url = 'https://jsonplaceholder.typicode.com/users/';
-//     fetch(url)
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((data) => {
-//       let authors = data;
-//     authors.map(function(author) {
-//       let li = document.createElement('li');
-//       let name = document.createElement('h2');
-//       let email = document.createElement('span');
-
-//       name.innerHTML = `${author.name}`;
-//       email.innerHTML = `${author.email}`;
-// console.log('test');
-//       li.appendChild(name);
-//       li.appendChild(email);
-//       list.appendChild(li);
-//       ul.appendChild(list);
-//       })
-//       })
-//       .catch(function(error) {
-//         console.log(error);
-//     })
-
-//     ul.appendChild(list);
+});
